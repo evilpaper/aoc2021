d69c5a6507a3518f234966a0cc9b0230f3787b4d
Pelle Lundgren <pellburt@gmail.com>
2021-12-05 09:25:40 +0100

    Start working on part two

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -10,7 +10,7 @@ const depths = readFileSync(
   .split("\n")
   .map((depth) => Number(depth));
 
-const amountIncreased = (input: number[]) =>
+export const timesIncreased = (input: number[]) =>
   input
     .map((depth: number, index: number) => {
       if (index === 0) return "N/A";
@@ -20,4 +20,32 @@ const amountIncreased = (input: number[]) =>
     })
     .filter((depth) => depth === "increased").length;
 
-console.log(amountIncreased(depths));
+// console.log(timesIncreased(depths));
+
+const spliceIntoChunks = (input: number[], chunkSize: number) => {
+  const result = [];
+  while (input.length > 0) {
+    const chunk = input.splice(0, chunkSize);
+    result.push(chunk);
+  }
+  return result;
+};
+
+const chunksOfThree = spliceIntoChunks(depths, 3);
+const sumsOfEachChunk = chunksOfThree.map((chunk) => {
+  return chunk.reduce((pre, cur) => pre + cur, 0);
+});
+const changesBetweenChunks = sumsOfEachChunk.map(
+  (chunk: number, index: number) => {
+    if (index === 0) return "N/A";
+    if (chunk > sumsOfEachChunk[index - 1]) return "increased";
+    if (chunk < sumsOfEachChunk[index - 1]) return "descreased";
+    return "no change";
+  }
+);
+
+const timesChunksIncreased = changesBetweenChunks.filter(
+  (item) => item === "increased"
+).length;
+
+console.log(chunksOfThree);
